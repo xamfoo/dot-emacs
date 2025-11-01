038c8ea16910fcbbf3889d55ee983b53764350f9
xamfoo <xamfoo@icloud.com>
2025-11-01 23:13:41 +0800

    Initial config

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dotemacs
+
 Emacs configuration
+
+## Usage
+
+**Initialization**
+
+```bash
+git clone git@github.com:xamfoo/dotemacs.git ~/.config/emacs
+```
---
+++ init.el
@@ -0,0 +1,32 @@
+;; Don't show the splash screen
+(setq inhibit-startup-message t)
+;; Turn off some unneeded UI elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+;; Display line numbers in every buffer
+(global-display-line-numbers-mode 1)
+;; Remembering recently edited files
+(recentf-mode 1)
+;; Save what you enter into minibuffer prompts
+(setq history-length 25)
+(savehist-mode 1)
+;; Remember and restore the last cursor location of opened files
+(save-place-mode 1)
+;; Move customization variables to a separate file and load it
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
+;; Don't pop up UI dialogs when prompting
+(setq use-dialog-box nil)
+;; Disable audible bell and enable visible bell
+(setq visible-bell t)
+;; Set default theme
+(load-theme 'modus-operandi t)
+;; Revert buffers when the underlying file has changed
+(global-auto-revert-mode 1)
+;; Initialize package management system and add MELPA repository
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
